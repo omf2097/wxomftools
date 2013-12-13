b26ce6ccbbcc9097226636f6e345cd69c6b1732e
Tuomas Virtanen <katajakasa@gmail.com>
2013-12-13 22:59:00 +0200

    Use wx-config for non-mingw.

--- cmake-scripts/Findwxwidgets3.cmake
+++ cmake-scripts/Findwxwidgets3.cmake
@@ -1,43 +1,59 @@
 
-SET(WXWIDGETS_SEARCH_PATHS
-    /usr/local/
-    /usr
-    /opt
-)
-
-FIND_PATH(WXWIDGETS_INCLUDE_DIR wx/wx.h 
-    HINTS ${WXWIDGETS_ROOT}
-    PATH_SUFFIXES include include/wx-3.0
-    PATHS ${WXWIDGETS_SEARCH_PATHS}
-)
-
-FIND_PATH(WXWIDGETS_SETUP_PATH wx/setup.h
-    HINTS ${WXWIDGETS_ROOT}
-    PATH_SUFFIXES 
-        lib/wx/include/msw-unicode-static-3.0/ 
-        lib/wx/include/msw-unicode-dynamic-3.0/
-    PATHS ${WXWIDGETS_SEARCH_PATHS}
-)
-
 IF(MINGW)
-    SET(SEARCH_LIBS wx_mswu_html-3.0 wx_mswu_adv-3.0 wx_mswu_aui-3.0 wx_mswu_core-3.0 wx_baseu-3.0 wxjpeg-3.0 wxpng-3.0 wxzlib-3.0 )
-ELSE(MINGW)
-    SET(SEARCH_LIBS wx_baseu-3.0)
-ENDIF(MINGW)
+    SET(WXWIDGETS_SEARCH_PATHS
+        /usr/local/
+        /usr
+        /opt
+    )
 
-FOREACH(SLIB ${SEARCH_LIBS})
+    FIND_PATH(WXWIDGETS_INCLUDE_DIR wx/wx.h 
+        HINTS ${WXWIDGETS_ROOT}
+        PATH_SUFFIXES include include/wx-3.0
+        PATHS ${WXWIDGETS_SEARCH_PATHS}
+    )
+
+    FIND_PATH(WXWIDGETS_SETUP_PATH wx/setup.h
+        HINTS ${WXWIDGETS_ROOT}
+        PATH_SUFFIXES 
+            lib/wx/include/msw-unicode-static-3.0/ 
+            lib/wx/include/msw-unicode-dynamic-3.0/
+        PATHS ${WXWIDGETS_SEARCH_PATHS}
+    )
+
+    SET(SEARCH_LIBS wx_mswu_html-3.0 wx_mswu_adv-3.0 wx_mswu_aui-3.0 wx_mswu_core-3.0 wx_baseu-3.0 wxjpeg-3.0 wxpng-3.0 wxzlib-3.0 )
+
+    FOREACH(SLIB ${SEARCH_LIBS})
     FIND_LIBRARY(FOUND_${SLIB}_LIB ${SLIB} HINTS ${WXWIDGETS_ROOT} PATH_SUFFIXES lib64 lib bin PATHS ${WXWIDGETS_SEARCH_PATHS} )
     SET(WXWIDGETS_LIBRARY ${WXWIDGETS_LIBRARY} ${FOUND_${SLIB}_LIB})
-ENDFOREACH(SLIB)
+    ENDFOREACH(SLIB)
+
+    SET(WXWIDGETS_INCLUDE_DIR ${WXWIDGETS_INCLUDE_DIR} ${WXWIDGETS_SETUP_PATH})
+ELSE(MINGW)
+    FIND_PROGRAM(WX_CONFIG
+        wx-config3.0 | wx-config2.9
+        PATHS
+            /usr/bin
+            /usr/local/bin
+    )
+
+    EXECUTE_PROCESS(
+        COMMAND WX_CONFIG "--cflags"
+        OUTPUT_VARIABLE WXWIDGETS_INCLUDE_DIR
+    )
+
+    EXECUTE_PROCESS(
+        COMMAND WX_CONFIG "--libs"
+        OUTPUT_VARIABLE WXWIDGETS_LIBRARY
+    )
+ENDIF(MINGW)
+
 
 IF(WXWIDGETS_INCLUDE_DIR AND WXWIDGETS_LIBRARY)
     SET(WXWIDGETS_FOUND TRUE)
 ENDIF (WXWIDGETS_INCLUDE_DIR AND WXWIDGETS_LIBRARY)
 
-SET(WXWIDGETS_INCLUDE_DIR ${WXWIDGETS_INCLUDE_DIR} ${WXWIDGETS_SETUP_PATH})
-
 IF(WXWIDGETS_FOUND)
-    MESSAGE(STATUS "Found wxWidgets 3.0: ${WXWIDGETS_LIBRARY} with setup.h at ${WXWIDGETS_SETUP_PATH}")
+    MESSAGE(STATUS "Found wxWidgets: ${WXWIDGETS_LIBRARY}")
 ELSE(WXWIDGETS_FOUND)
-    MESSAGE(WARNING "Could not find wxWidgets 3.0")
+    MESSAGE(WARNING "Could not find wxWidgets")
 ENDIF(WXWIDGETS_FOUND)
